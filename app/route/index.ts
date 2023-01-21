dc98e5f11643fd0f6d4c45afd8fbca421785ca60
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-01-21 16:42:23 +0800

    Add routes

---
+++ app/route/index.ts
@@ -0,0 +1,5 @@
+import usersRoute from './users/users.route';
+import authRoute from './auth/auth.route';
+import rolesRoute from './roles/roles.route';
+
+export { usersRoute, authRoute, rolesRoute }
